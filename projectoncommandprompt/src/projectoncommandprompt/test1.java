8cf0631ac6ad042ea58149435bfa590967f51f3c
ankitabash <niitz.man@gmail.com>
2017-10-27 02:30:49 -0400

    now

---
+++ projectoncommandprompt/src/projectoncommandprompt/test1.java
@@ -0,0 +1,14 @@
+package projectoncommandprompt;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+@Test
+	public void help()
+	{
+		System.out.println("hello");
+		
+	}
+	
+	
+}
---
+++ projectoncommandprompt/src/projectoncommandprompt/test2.java
@@ -0,0 +1,15 @@
+package projectoncommandprompt;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+	
+	
+	@Test
+	public void help2()
+	{
+		System.out.println("hello from help2");
+		
+	}
+
+}
